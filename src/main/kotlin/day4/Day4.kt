465dee7e6fdabf7aca3a521312ab9fbabf2f2ef9
Linus Weiss <weisslinus@gmail.com>
2024-12-04 08:30:38 +0100

    Change Day4 function order

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -30,22 +30,6 @@ private enum class Direction {
     TOP, BOTTOM, LEFT, RIGHT, TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT;
 }
 
-private fun part1(): Int {
-    val matrix = getMatrix()
-    var xmasCount = 0
-    matrix.forEachIndexed { row, line ->
-        line.forEachIndexed { column, char ->
-            if (char == 'X') {
-                xmasCount += Direction.entries
-                    .map { direction -> part1CheckForXmas(matrix, row, column, direction, "X") }
-                    .count { it }
-            }
-        }
-    }
-
-    return xmasCount
-}
-
 private fun getNextRowForDirection(row: Int, direction: Direction): Int {
     return when (direction) {
         TOP, TOP_LEFT, TOP_RIGHT -> row - 1
@@ -77,6 +61,22 @@ private fun getCharInDirection(
     if (nextColumn > matrix.first().lastIndex) return null
 
     return Triple(matrix[nextRow][nextColumn], nextRow, nextColumn)
+}
+
+private fun part1(): Int {
+    val matrix = getMatrix()
+    var xmasCount = 0
+    matrix.forEachIndexed { row, line ->
+        line.forEachIndexed { column, char ->
+            if (char == 'X') {
+                xmasCount += Direction.entries
+                    .map { direction -> part1CheckForXmas(matrix, row, column, direction, "X") }
+                    .count { it }
+            }
+        }
+    }
+
+    return xmasCount
 }
 
 private fun part1CheckForXmas(
