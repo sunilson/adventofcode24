c99b1e97aa657626dfbd5fe7a09f533e30e4219f
Linus Weiss <weisslinus@gmail.com>
2024-12-05 09:27:02 +0100

    Add Day5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@ import day1.day1
 import day2.day2
 import day3.day3
 import day4.day4
+import day5.day5
 
 fun main() {
     day1()
     day2()
     day3()
     day4()
+    day5()
 }
---
+++ src/main/kotlin/day5/Day5.kt
@@ -0,0 +1,67 @@
+package day5
+
+import utils.runDay
+import utils.runPart
+
+
+fun day5() {
+    runDay(5) {
+        runPart(1) { part1() }
+        runPart(2) { part2() }
+    }
+}
+
+private fun part1(): Int {
+    val orderingRules = getOrderingRules()
+    val updates = getUpdates()
+    val sortedUpdates = updates.map { update -> update.toMutableList().orderUpdate(orderingRules) }
+    val correctUpdates = updates.filterIndexed { index, update -> sortedUpdates[index] == update }
+    return correctUpdates.sumOf { it[(it.size / 2)] }
+}
+
+fun part2(): Int {
+    val orderingRules = getOrderingRules()
+    val updates = getUpdates()
+    val sortedUpdates = updates.map { update -> update.toMutableList().orderUpdate(orderingRules) }
+    val incorrectUpdates = sortedUpdates.filterIndexed { index, update -> updates[index] != update }
+    return incorrectUpdates.sumOf { it[(it.size / 2)] }
+}
+
+private fun getOrderingRules(): Map<Int, Set<Int>> {
+    val input = INPUT_DAY_5.split("\n\n").first().trim()
+    val orderRules = mutableMapOf<Int, Set<Int>>()
+
+    input.split("\n")
+        .forEach { line ->
+            val (before, after) = line.split("|").map { it.toInt() }
+            val existingSet = orderRules[before] ?: mutableSetOf()
+            orderRules[before] = existingSet.plus(after)
+        }
+
+    return orderRules
+}
+
+private fun getUpdates(): List<List<Int>> {
+    val input = INPUT_DAY_5.split("\n\n").last().trim()
+    return input.split("\n")
+        .map { line -> line.split(",").map { it.toInt() } }
+}
+
+private fun MutableList<Int>.orderUpdate(orderingRules: Map<Int, Set<Int>>): List<Int> {
+    val newList = mutableListOf<Int>()
+    val relevantOrderingRules = orderingRules.filter { this.contains(it.key) }.toMutableMap()
+    val initialSize = size
+
+    while (newList.size < initialSize) {
+        val beforeAllOther = first { number ->
+            relevantOrderingRules.filter { rule ->
+                rule.key != number && rule.value.contains(number)
+            }.isEmpty()
+        }
+        newList.add(beforeAllOther)
+        remove(beforeAllOther)
+        relevantOrderingRules.remove(beforeAllOther)
+    }
+
+    return newList
+}
