1e5e83f7563961cda81ef0a38b76e19c739c9500
Linus Weiss <weisslinus@gmail.com>
2024-12-08 16:13:56 +0100

    Day 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,14 @@ import day3.day3
 import day4.day4
 import day5.day5
 import day6.day6
+import day7.day7
 
 fun main() {
     day1()
     day2()
     day3()
     day4()
+    day5()
     day6()
+    day7()
 }
---
+++ src/main/kotlin/day7/Day7.kt
@@ -0,0 +1,98 @@
+package day7
+
+import day7.Operator.*
+import kotlinx.coroutines.*
+import utils.runDay
+import utils.runPart
+
+
+fun day7() {
+    runDay(7) {
+        runPart(1) { part(possibleOperators = listOf(ADD, MULTIPLY)) }
+        runPart(2) { part(possibleOperators = listOf(ADD, MULTIPLY, COMBINE)) }
+    }
+}
+
+private fun part(possibleOperators: List<Operator>) = runBlocking(Dispatchers.Default) {
+    val equations = getEquations()
+    val allPossiblePermutations = mutableMapOf<Int, List<List<Operator>>>()
+    equations.mapParallel { (expectedResult, equationParts) ->
+        val possiblePermutations =
+            allPossiblePermutations.getOrElse(equationParts.size) { emptyList() }.toMutableList()
+
+        fun calculatePossiblePermutations(operators: List<Operator>, depth: Int) {
+            if (depth == 0) {
+                possiblePermutations.add(operators)
+            } else {
+                possibleOperators.forEach {
+                    calculatePossiblePermutations(operators + it, depth - 1)
+                }
+            }
+        }
+
+        if (possiblePermutations.isEmpty()) {
+            calculatePossiblePermutations(emptyList(), equationParts.size)
+            allPossiblePermutations[equationParts.size] = possiblePermutations
+        }
+
+        possiblePermutations.forEach { permutation ->
+            if (
+                solveEquation(
+                    expectedResult = expectedResult,
+                    equation = buildList {
+                        equationParts.forEachIndexed { index, number ->
+                            add(number)
+                            add(permutation[index])
+                        }
+                    }.dropLast(1)
+                )
+            ) {
+                return@mapParallel expectedResult
+            }
+        }
+
+        0L
+    }.sum()
+}
+
+private suspend fun <T, R> List<T>.mapParallel(block: (T) -> R): List<R> {
+    return coroutineScope {
+        map { async { block(it) } }.map { it.await() }
+    }
+}
+
+private fun solveEquation(
+    expectedResult: Long,
+    equation: List<Any>
+): Boolean {
+    var result = equation.first() as Long
+    var index = 1
+    while (index <= equation.size) {
+        val operator = equation.getOrNull(index) as? Operator ?: break
+        val nextNumber = equation[index + 1] as Long
+        result = when (operator) {
+            ADD -> result + nextNumber
+            MULTIPLY -> result * nextNumber
+            COMBINE -> (result.toString() + nextNumber.toString()).toLong()
+        }
+        index += 2
+    }
+
+    return result == expectedResult
+}
+
+enum class Operator {
+    ADD, MULTIPLY, COMBINE
+}
+
+private fun getEquations(): List<Pair<Long, List<Long>>> {
+    return INPUT_DAY_7.split("\n")
+        .map { line ->
+            val result = line.substringBefore(":").toLong()
+            val parts = line.substringAfter(":")
+                .trim()
+                .split(" ")
+                .map { it.toLong() }
+            result to parts
+        }
+}
